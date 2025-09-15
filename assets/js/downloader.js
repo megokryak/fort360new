d8cc4307048748dd88ac762f435870d7c381cee4
Vladimir Sviridov <vsviridov80@gmail.com>
2025-09-16 01:50:56 +0300

    fix download prez

--- assets/js/downloader.js
+++ assets/js/downloader.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('downloadPrezButton').addEventListener('click', function() {
       const link = document.createElement('a');
-      link.href = '/assets/docs/fort360_prezentation_2025.pdf';
+      link.href = '/assets/images/fort360_prezentation_2025.pdf';
       link.download = 'fort360_prezentation_2025.pdf';
       document.body.appendChild(link);
       link.click();
